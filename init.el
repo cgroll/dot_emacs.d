713a033f84daa5ee21dc86ad7c91cbc0df926e99
chris <chris@chris-wk.(none)>
2013-12-20 17:04:18 +0100

    new matlab features included, bbdb should automatically get new adresses

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;(require 'org-install)
+                                        ;(require 'org-install)
 (require 'org)
 (require 'use-package)                  ; enables autoloading of functions
 
@@ -62,7 +62,7 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
- 
+
 ;; ido
 ;; (require 'ido)                          
 (setq ido-enable-flex-matching t)
@@ -106,6 +106,7 @@
    (bbdb-initialize 'gnus 'message)
    )
 
+(setq bbdb-always-add-addresses t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;;;       auto-complete-mode and yasnippet
@@ -125,22 +126,22 @@
 (global-set-key (kbd "C-i") 'yas-expand)
 
 (define-key yas-minor-mode-map (kbd "C-i") 'yas-expand)
-;(define-key yas-minor-mode-map (kbd "C-u") 'yas-next-field)
-;(define-key yas-minor-mode-map (kbd "C-U") 'yas-prev-field)
+                                        ;(define-key yas-minor-mode-map (kbd "C-u") 'yas-next-field)
+                                        ;(define-key yas-minor-mode-map (kbd "C-U") 'yas-prev-field)
 (define-key yas-minor-mode-map (kbd "C-S-i") 'yas-prev-field)
-; REMARK: it behaves completely different than what the settings say:
-; C-u is yas-prev-field, while TAB is yas-next-field
+                                        ; REMARK: it behaves completely different than what the settings say:
+                                        ; C-u is yas-prev-field, while TAB is yas-next-field
 
 (use-package auto-complete-config
    :config
    (progn
-      ;(ac-set-trigger-key "C-o")
+                                        ;(ac-set-trigger-key "C-o")
       (bind-key "C-o" 'ac-expand ac-mode-map)
       (bind-key "C-o" 'ac-complete) 
-      ;(unbind-key "M-/")
-      ;(bind-key "M-/" 'ac-stop)
+                                        ;(unbind-key "M-/")
+                                        ;(bind-key "M-/" 'ac-stop)
       )
-
+   
    )
 
 (org-babel-load-file "~/.emacs.d/init-all.org")
@@ -150,12 +151,12 @@
 (bind-key "C-o" 'ac-expand ac-mode-map)
 
 
-;(require 'auto-complete)
+                                        ;(require 'auto-complete)
 
 
-;(require 'yasnippet)
-;(global-set-key (kbd "C-o") 'yas-prev-field)
-;(define-key yas-minor-mode-map (kbd "C-o") 'yas-expand)
+                                        ;(require 'yasnippet)
+                                        ;(global-set-key (kbd "C-o") 'yas-prev-field)
+                                        ;(define-key yas-minor-mode-map (kbd "C-o") 'yas-expand)
 
 
 ;; Add ac-source-dictionary to ac-sources of all buffer
@@ -163,7 +164,7 @@
    "allow manually adding etags-source, so that it is not loaded from
 startup"
    (interactive)
-  (setq ac-sources (append ac-sources '(ac-source-etags ac-source-R))))
+   (setq ac-sources (append ac-sources '(ac-source-etags ac-source-R))))
 
 
 
@@ -208,7 +209,7 @@ startup"
    (error nil))
 
 ;; load tags-table, so that ac-source-etags will not fail
- (visit-tags-table "~/Dropbox/research_current_ntb_head/TAGS")
+(visit-tags-table "~/Dropbox/research_current_ntb_head/TAGS")
 ;; (setq inferior-julia-program-name
 ;;    "~/julia/usr/bin/julia-release-basic")
 
@@ -232,6 +233,17 @@ startup"
 (fset 'eval-variable
    [?\C-x ?\C-b ?* ?M ?A ?T ?L ?A ?B ?* return ?\C-y return ?\M-x ?c ?o ?m ?i ?n ?t ?- ?p ?r ?e ?v ?i tab ?p ?r ?o tab return ?\C-l ?\M-x ?c ?o ?m ?i ?n ?t ?- ?n ?e ?x ?- backspace ?t ?- ?p tab return ?\C-x ?o])
 
+(defun cg/matlab-eval-paragraph ()
+   "equivalent to ESS eval paragraph"
+   (interactive)
+   (forward-paragraph)
+   (backward-paragraph)
+   (push-mark)
+   (forward-paragraph)
+   (matlab-shell-run-region (mark) (point))
+   (keyboard-quit)
+   )
+
 (defun cg/modify-matlab-keybindings ()
    "adapt matlab-emacs keybindings"
    (bind-key "M-j" 'cg/insert-tab matlab-mode-map)
@@ -239,9 +251,23 @@ startup"
    (bind-key "C-#" 'comment-or-uncomment-line matlab-mode-map)
    (bind-key "M-;" 'comment-dwim matlab-mode-map)
    (bind-key "C-c C-v" 'eval-variable matlab-mode-map)
-   (bind-key "C-c C-g" 'eval-whos-on-variable matlab-mode-map)
+   ;; (bind-key "C-c C-g" 'eval-whos-on-variable matlab-mode-map)
+   (bind-key "C-c C-g" 'cg/matlab-display-variable matlab-mode-map)
    (bind-key "C-c C-n" 'eval-line-and-go matlab-mode-map)
-   (bind-key "C-c C-p" 'eval-cell-and-go matlab-mode-map)
+   ;; (bind-key "C-c C-p" 'eval-cell-and-go matlab-mode-map)
+   (bind-key "C-c C-p" 'cg/matlab-eval-paragraph matlab-mode-map)
+   (bind-key "C-c C-b" 'matlab-shell-run-cell)
+   )
+
+(defun cg/matlab-display-variable (variable)
+   "display variable information"
+   (interactive (list (read-from-minibuffer
+                         "MATLAB variable: "
+                         (cons (matlab-read-word-at-point) 0))))
+   ;; (cons (cg/copy-current-symbol) 0)    
+   ;; (setq matlab-variable (cons (matlab-read-word-at-point) 0))
+   (setq matlab-command (concat "str(" (concat variable) ")"))
+   (matlab-shell-run-command matlab-command)
    )
 
 ;; allow matlab to be loaded through call to matlab-mode or
@@ -250,7 +276,7 @@ startup"
 (use-package matlab
    :defer t
    :load-path "~/matlab-emacs"   
-;;   :if (file-exists-p "/home/chris/MATLAB/R2012a/bin/matlab")
+   ;;   :if (file-exists-p "/home/chris/MATLAB/R2012a/bin/matlab")
    :commands (matlab-shell matlab-mode)
    :mode ("\\.m$" . matlab-mode)
    :init
@@ -261,11 +287,13 @@ startup"
    :config
    (progn
       (require 'matlab-load)
-      ;(matlab-cedet-setup)
+                                        ;(matlab-cedet-setup)
       (add-to-list 'matlab-mode-hook 'cg/modify-current-syntax-table)
       (add-to-list 'matlab-mode-hook 'cg/modify-matlab-keybindings)
       (add-to-list 'matlab-mode-hook 'cg/command-line-keybindings)
-      (setq matlab-shell-command "/home/chris/MATLAB/R2012a/bin/matlab")
+      ;; (setq matlab-shell-command
+      ;; "/home/chris/MATLAB/R2012a/bin/matlab")
+      (setq matlab-shell-command "matlab")
       )
    )
 
@@ -301,7 +329,7 @@ startup"
 (add-to-list 'load-path "~/.emacs.d/extensions/ess-13.09/lisp")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/ess-13.09/lisp/")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/ess-13.09-2/lisp/")
-;(require 'ess-site)
+                                        ;(require 'ess-site)
 
 ;; allow julia to be loaded through call to julia-mode or
 ;; ess-inferior process
@@ -392,17 +420,17 @@ startup"
       (require 'ess-R-object-tooltip)
       (define-key ess-mode-map (kbd "C-c 1") 'r-show-head)
       (define-key ess-mode-map (kbd "C-c 2") 'r-show-str)
-
+      
       )
    )
 
-  
- ; (define-key ess-mode-map (kbd "C-M--") 'cg/R-assign)
-;  (define-key ess-mode-map (kbd "_") 'self-insert-command)
-;  (define-key inferior-ess-mode-map (kbd "C-M--") 'cg/R-assign)
-;  (define-key inferior-ess-mode-map (kbd "_") 'self-insert-command)
-;  (global-set-key (kbd "_") 'self-insert-command)
- ; (global-set-key (kbd "C-M--") 'cg/R-assign)
+
+                                        ; (define-key ess-mode-map (kbd "C-M--") 'cg/R-assign)
+                                        ;  (define-key ess-mode-map (kbd "_") 'self-insert-command)
+                                        ;  (define-key inferior-ess-mode-map (kbd "C-M--") 'cg/R-assign)
+                                        ;  (define-key inferior-ess-mode-map (kbd "_") 'self-insert-command)
+                                        ;  (global-set-key (kbd "_") 'self-insert-command)
+                                        ; (global-set-key (kbd "C-M--") 'cg/R-assign)
 
 
 
@@ -422,8 +450,8 @@ startup"
 (add-to-list 'load-path "~/.emacs.d/extensions/gnuplot")
 
 ;; these lines enable the use of gnuplot mode
-  (autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
-  (autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
+(autoload 'gnuplot-mode "gnuplot" "gnuplot major mode" t)
+(autoload 'gnuplot-make-buffer "gnuplot" "open a buffer in gnuplot mode" t)
 
 ;; load the file
 ;; (add-to-list 'load-path "/usr/bin")
@@ -480,10 +508,10 @@ startup"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'gnus)
-;(org-babel-load-file "~/.emacs.d/init-gnus.org")
-  ;; (add-to-list 'load-path "~/.emacs.d/extensions/bbdb/lisp/")
-  ;; (require 'bbdb)
-  ;; (bbdb-initialize 'gnus 'message)
+                                        ;(org-babel-load-file "~/.emacs.d/init-gnus.org")
+;; (add-to-list 'load-path "~/.emacs.d/extensions/bbdb/lisp/")
+;; (require 'bbdb)
+;; (bbdb-initialize 'gnus 'message)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -542,9 +570,9 @@ startup"
 (use-package google-translate
    :defer t
    :commands (google-translate-query-translate-reverse
-               google-translate-query-translate
-               google-translate-at-point
-               google-translate-at-point-reverse)
+                google-translate-query-translate
+                google-translate-at-point
+                google-translate-at-point-reverse)
    :init
    (progn
       (bind-key "C-t l" 'google-translate-query-translate-reverse)
@@ -637,62 +665,62 @@ startup"
 
 ;; new block
 (defun new-block ()
-  "Create new block header at point"
-  (interactive)
-  (setq counter 1)
-  (while (< counter 60)
-    (insert comment-start)
-    (incf counter))
-  (newline)
-  (setq counter 1)
-  (while (< counter 4)
-    (insert comment-start)
-    (incf counter))
-  (newline)
-  (setq counter 1)
-  (while (< counter 6)
-    (insert comment-start)
-    (incf counter))
-  (setq counter 1)
-  (while (< counter 8)
-    (insert " ")
-    (incf counter))
-  (push-mark)
-  (deactivate-mark)
-  (newline)
-  (setq counter 1)
-  (while (< counter 4)
-    (insert comment-start)
-    (incf counter))
-  (newline)
-  (setq counter 1)
-  (while (< counter 60)
-    (insert comment-start)
-    (incf counter))
-  (newline)
-  (exchange-point-and-mark))
+   "Create new block header at point"
+   (interactive)
+   (setq counter 1)
+   (while (< counter 60)
+      (insert comment-start)
+      (incf counter))
+   (newline)
+   (setq counter 1)
+   (while (< counter 4)
+      (insert comment-start)
+      (incf counter))
+   (newline)
+   (setq counter 1)
+   (while (< counter 6)
+      (insert comment-start)
+      (incf counter))
+   (setq counter 1)
+   (while (< counter 8)
+      (insert " ")
+      (incf counter))
+   (push-mark)
+   (deactivate-mark)
+   (newline)
+   (setq counter 1)
+   (while (< counter 4)
+      (insert comment-start)
+      (incf counter))
+   (newline)
+   (setq counter 1)
+   (while (< counter 60)
+      (insert comment-start)
+      (incf counter))
+   (newline)
+   (exchange-point-and-mark))
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(LaTeX-math-abbrev-prefix "M-g")
- '(calendar-latitude 48.139)
- '(calendar-longitude 11.58)
- '(global-auto-complete-mode t)
- '(matlab-shell-command "~/remote_matlab")
- '(org-agenda-show-all-dates t)
- '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-start-on-weekday nil)
- '(org-clock-into-drawer t)
- '(org-deadline-warning-days 14)
- '(org-drill-optimal-factor-matrix (quote ((3 (2.46 . 2.46) (1.56 . 2.2) (2.04 . 2.244) (2.08 . 2.166) (2.6 . 2.6) (1.96 . 2.316) (2.2199999999999998 . 2.379) (1.9000000000000001 . 2.111) (1.8199999999999998 . 2.26) (2.5 . 2.5) (2.36 . 2.439)) (2 (2.08 . 2.166) (1.8199999999999998 . 2.26) (2.2199999999999998 . 2.379) (2.22 . 2.22) (2.46 . 2.497) (1.7000000000000002 . 2.255) (2.04 . 2.319) (1.96 . 2.238) (2.1799999999999997 . 2.325) (2.36 . 2.439) (2.5 . 2.5) (2.6 . 2.588) (1.56 . 2.2)) (1 (2.08 . 3.902) (1.8199999999999998 . 3.615) (1.56 . 3.52) (2.2199999999999998 . 3.806) (2.04 . 3.59) (2.36 . 3.902) (2.1799999999999997 . 3.804) (2.5 . 4.0) (2.6 . 4.14) (1.96 . 3.706) (1.7000000000000002 . 3.608)))))
- '(org-fast-tag-selection-single-key nil)
- '(org-format-latex-options (quote (:foreground default :background default :scale 1.7 :html-foreground "Black" :html-background "Transparent" :html-scale 2.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\["))))
- '(org-reverse-note-order nil)
- '(safe-local-variable-values (quote ((org-src-preserve-indentation . t) (TeX-master . evt_main\.tex) (org-export-babel-evaluate . t) (org-export-publishing-directory . "./src_results/") (org-export-babel-evaluate . no-export)))))
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(LaTeX-math-abbrev-prefix "M-g")
+   '(calendar-latitude 48.139)
+   '(calendar-longitude 11.58)
+   '(global-auto-complete-mode t)
+   '(matlab-shell-command "~/remote_matlab")
+   '(org-agenda-show-all-dates t)
+   '(org-agenda-skip-deadline-if-done t)
+   '(org-agenda-skip-scheduled-if-done t)
+   '(org-agenda-start-on-weekday nil)
+   '(org-clock-into-drawer t)
+   '(org-deadline-warning-days 14)
+   '(org-drill-optimal-factor-matrix (quote ((3 (2.46 . 2.46) (1.56 . 2.2) (2.04 . 2.244) (2.08 . 2.166) (2.6 . 2.6) (1.96 . 2.316) (2.2199999999999998 . 2.379) (1.9000000000000001 . 2.111) (1.8199999999999998 . 2.26) (2.5 . 2.5) (2.36 . 2.439)) (2 (2.08 . 2.166) (1.8199999999999998 . 2.26) (2.2199999999999998 . 2.379) (2.22 . 2.22) (2.46 . 2.497) (1.7000000000000002 . 2.255) (2.04 . 2.319) (1.96 . 2.238) (2.1799999999999997 . 2.325) (2.36 . 2.439) (2.5 . 2.5) (2.6 . 2.588) (1.56 . 2.2)) (1 (2.08 . 3.902) (1.8199999999999998 . 3.615) (1.56 . 3.52) (2.2199999999999998 . 3.806) (2.04 . 3.59) (2.36 . 3.902) (2.1799999999999997 . 3.804) (2.5 . 4.0) (2.6 . 4.14) (1.96 . 3.706) (1.7000000000000002 . 3.608)))))
+   '(org-fast-tag-selection-single-key nil)
+   '(org-format-latex-options (quote (:foreground default :background default :scale 1.7 :html-foreground "Black" :html-background "Transparent" :html-scale 2.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\["))))
+   '(org-reverse-note-order nil)
+   '(safe-local-variable-values (quote ((org-src-preserve-indentation . t) (TeX-master . evt_main\.tex) (org-export-babel-evaluate . t) (org-export-publishing-directory . "./src_results/") (org-export-babel-evaluate . no-export)))))
 ;; (custom-set-faces
 ;;   ;; custom-set-faces was added by Custom.
 ;;   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -704,15 +732,15 @@ startup"
 ;;  '(org-level-4 ((t (:inherit outline-4 :foreground "deep sky blue" :height 1.2))))
 ;;  '(org-level-5 ((t (:inherit outline-5 :foreground "deep pink")))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :foreground "yellow" :height 1.1 :width expanded))))
- '(org-level-2 ((t (:inherit outline-2 :foreground "orange" :height 1.1 :width normal))))
- '(org-level-3 ((t (:inherit outline-3 :foreground "green yellow" :height 1.1 :width normal))))
- '(org-level-4 ((t (:inherit outline-4 :foreground "deep sky blue" :height 1.1))))
- '(org-level-5 ((t (:inherit outline-5 :foreground "deep pink")))))
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(org-level-1 ((t (:inherit outline-1 :foreground "yellow" :height 1.1 :width expanded))))
+   '(org-level-2 ((t (:inherit outline-2 :foreground "orange" :height 1.1 :width normal))))
+   '(org-level-3 ((t (:inherit outline-3 :foreground "green yellow" :height 1.1 :width normal))))
+   '(org-level-4 ((t (:inherit outline-4 :foreground "deep sky blue" :height 1.1))))
+   '(org-level-5 ((t (:inherit outline-5 :foreground "deep pink")))))
 (put 'upcase-region 'disabled nil)
 
 
